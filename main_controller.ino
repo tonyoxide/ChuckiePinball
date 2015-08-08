c966962b31c33ea2d1e0a84a3aa15468eaea68ec
Tony Oxide <tonyoxide@users.noreply.github.com>
2015-08-08 18:43:46 -0500

    Remove delays from LED lighting Fix cross fade time so it's variable now Stuff

--- main_controller.ino
+++ main_controller.ino
@@ -1,5 +1,5 @@
 /*
- BM 2015 Chuckie Pinball by TonyOxide
+ BM 2015 Chuckie Pinball by RCI and TonyOxide
  This code is in the public domain.
  Version 0.03
  
@@ -9,7 +9,13 @@
 
 // Turn debugging on or off...
 // Don't even try to use #if syntax, the IDE breaks if you do
-char TDEBUG = 3;
+// Bitmask 
+// 1 = LEDs
+// 2 = PIRs
+// 4 = FINGERs
+// 8 = 
+
+char TDEBUG = 1;
 
 unsigned char pin_led_test = 13;
 unsigned char pin_sen_pir_test = 7;
@@ -23,21 +29,23 @@ unsigned char pin_face_sen = 14;    // Face sensor pin
 unsigned long delay_red_green_min = 5000; // minimum time red/green is on
 unsigned long delay_red_green_max = 10000; // maximum time red/green is on
 unsigned long delay_fade = 2000;    // Fade between red/green/uv
-unsigned long delay_led_cross = 10; // Minimum amount of time to increment 1 crossfade value in ms
-unsigned long random_uv_led_pool = 2000; // 1 out of every xx seconds, light the UV LED instead
-unsigned char uv_led_on_time = 150; // Time to flash UV LED
+float delay_led_cross = 600; // Minimum amount of time to increment 1 crossfade value in ms
+unsigned long random_uv_led_pool = 3000; // 1 out of every xx seconds, light the UV LED instead
+unsigned char uv_led_on = 850; // Time to flash UV LED
 unsigned char uv_led_on_buffer = 0; // Time to turn off all other LEDS before/after UV LED
+unsigned long uv_led_on_time = 0; // Time that the UV LED was turned on
 unsigned int sen_light_min = 900;   // Minimum value for light sensor
 unsigned char loop_delay = 2;      // Default loop delay in ms
 
 // Variables you probably don't want to change
 unsigned long ctr_time; // Time since boot in ms
 unsigned long ctr_time_prv; // Time of the start of the previous loop
-
+unsigned int time_loop_delay = 0; // Delay of last loop run
 unsigned char pin_led_cross_on = pin_led_red;  // Crossfade on/off value
 unsigned char pin_led_cross_off = pin_led_green; // Crossfade on/off value
-unsigned long led_level_change = 0; // Value to change for LED
+float led_level_change = 0; // Value to change for LED
 unsigned long led_level_change_prv = 0; // Last time the LED level changed
+unsigned long led_level_change_start = 0; // Fade start time
 
 unsigned char pin_sen_light = A5;  // Light sensor pin
 int led_cross_on = 0; // Crossfade level for LED turning on
@@ -60,7 +68,6 @@ unsigned int ctr_red = 1;   // Number of red activations (
 unsigned int ctr_green = 0; // Number of green activations
 unsigned int ctr_uv = 0;    // Number of UV activations
 unsigned int ctr_loop = 0;  // Number of loops
-unsigned int time_loop_delay = 0;
 
 unsigned long time_led_start = 0;  // Time LED went on
 
@@ -88,10 +95,10 @@ int pir_check(int pin = 0) {
   } else {
     digitalWrite(pin_led_test,HIGH);
   }
-  if (TDEBUG >= 2) {
+  if (TDEBUG & 2) {
       Serial.print("pir=");
       Serial.println(val);
-      delay(100);
+      delay(10);
   }
 }
 
@@ -117,7 +124,7 @@ void setup() {
   Serial.begin(4800);
   Serial.write(232);
   delay(50);
-
+  led_test();
   if (TDEBUG == 1) {
     Serial.begin(9600);
     pinMode(pin_sen_pir_test, INPUT);
@@ -126,8 +133,7 @@ void setup() {
 }
 
 void loop() {
-
-  // Housekeeping
+	// Housekeeping
   ctr_time_prv = ctr_time; // Set the previous value of the timer
   ctr_time = millis();
 	time_loop_delay = ctr_time - ctr_time_prv; // Delay since the last loop
@@ -183,20 +189,19 @@ void loop() {
 void panel_checkFade() { // See if we should fade to another LED for the panel
 	led_on_time = ctr_time - time_led_start;
 	// See if we need to flash the UV LED
-	if (random(random_uv_led_pool) == 1) { // Randomly display UV LED
-		// Turn off the other LEDs
-//		 analogWrite(pin_led_cross_on, 0);
-//		 analogWrite(pin_led_cross_off, 0);
-		 delay(uv_led_on_buffer);
+	if (random(random_uv_led_pool) == 1 || uv_led_on_time > 0) { // Randomly display UV LED
+		if (uv_led_on_time > 0) { // UV is currently on
+			// See if we should turn it off
+			if (ctr_time - uv_led_on_time > uv_led_on) { // on long enough, turn it off
+				analogWrite(pin_led_uv, 0);
+				uv_led_on_time = 0;
+			}
+		} else {
 		 // Turn on UV LED
-		 analogWrite(pin_led_uv, led_bright_uv);
-		 delay(uv_led_on_time);
-		 // Turn off UV LED
-		 analogWrite(pin_led_uv, 0);
-		 delay(uv_led_on_buffer);
-//		 analogWrite(pin_led_cross_on, led_cross_on);
-//		 analogWrite(pin_led_cross_off, led_cross_off);
-		 ctr_uv++;
+			uv_led_on_time = ctr_time;
+			analogWrite(pin_led_uv, led_bright_uv);
+			ctr_uv++;
+		}		 
 	}
   
   if ( led_on_time > delay_red_green_random) { // Time to switch to the alternate LED
@@ -213,7 +218,7 @@ void panel_checkFade() { // See if we should fade to another LED for the panel
 		led_bright_red = light_on_brightness * 1;
 		led_bright_green = light_on_brightness * 1;
 	  led_bright_uv = light_on_brightness * 1;
-
+		
 		if ( ctr_red > ctr_green) {
 			pin_led_cross_on = pin_led_green;
 			led_cross_off = led_bright; // Set cross-fade max value for LED turning off
@@ -225,13 +230,15 @@ void panel_checkFade() { // See if we should fade to another LED for the panel
 			led_bright = led_bright_red;
 			ctr_red++;
 		}
-
+		
     led_cross_on = 0;  // Set cross-fade value to 0 for the LED turning on
-    led_level_change_prv = millis();
-		if (TDEBUG >= 1) {
+    led_level_change_start = millis();
+		if (TDEBUG & 1) {
 			Serial.print("led_bright= ");
-			Serial.println(led_bright);
-			Serial.println(led_bright_red);
+			Serial.print(led_bright);
+			Serial.print("\tled_red= ");
+			Serial.print(led_bright_red);
+			Serial.print("\tled_green= ");
 			Serial.println(led_bright_green);
 		}
 
@@ -239,37 +246,45 @@ void panel_checkFade() { // See if we should fade to another LED for the panel
     if (led_cross_on < led_bright || led_cross_off != 0) { // LED to turn on is not yet at full brightness, increase by 1 level
       // See how many levels to change the LED
 			
-      led_level_change = (millis() - led_level_change_prv) / delay_led_cross;
-      if (led_level_change == 0) { // Haven't waited long enough... return
-        return;
+			ctr_time = millis();
+			// Delay between last change and now
+      led_level_change = (ctr_time - led_level_change_start) / delay_led_cross;
+      if (led_level_change > 1) { // Too long, turn them off/on
+        led_level_change = 1;
       }
-      led_level_change_prv = millis();
-      led_cross_on += led_level_change;
-      led_cross_off -= led_level_change;
+			
+      led_cross_on = led_level_change * led_bright;
+      led_cross_off = led_bright - (led_level_change * led_bright);
 
-      if (led_cross_on > led_bright || led_cross_off < 0) {  // Values too big or too small, just punt
+      if (led_cross_on >= led_bright || led_cross_off < 0) {  // Values too big or too small, just punt
         led_cross_on = led_bright;
         led_cross_off = 0;
       }
-
+			
       analogWrite(pin_led_cross_on, led_cross_on);
       analogWrite(pin_led_cross_off, led_cross_off);
       // print the results to the serial monitor:
-			if (TDEBUG == 1) {
-				Serial.print("ledon=");
+			if (TDEBUG & 1) {
+				Serial.print("t=");
+				Serial.print(ctr_time);
+				Serial.print("\tst=");
+				Serial.print(led_level_change_start);
+				Serial.print("\tled_bright=");
+				Serial.print(led_bright);
+				Serial.print("\tledon=");
 				Serial.print(led_cross_on);
 				Serial.print("\tledoff=");
 				Serial.print(led_cross_off);
 				Serial.print("\tledlevel=");
 				Serial.print(led_level_change);
 				Serial.print("\t pin=");
-				Serial.println(pin_led_cross_on);
-				delay(200);
+				Serial.print(pin_led_cross_on);
+				Serial.print("\t chg=");
+				Serial.println((ctr_time - led_level_change_start) / delay_led_cross);
+				delay(20);
 			}
     }
-
   }
-
 }
 
 //Reads finger inputs from mux into array
@@ -455,3 +470,16 @@ unsigned int readMuxPin(unsigned char muxPin){
 
   return result;
 }
+
+// Test functions
+void led_test() { // Startup test for LEDs
+	analogWrite(pin_led_red, 255);
+  analogWrite(pin_led_green, 255);
+  analogWrite(pin_led_uv, 255);
+	delay(5000);
+	analogWrite(pin_led_red, 0);
+  analogWrite(pin_led_green, 0);
+  analogWrite(pin_led_uv, 0);
+	
+}
+
